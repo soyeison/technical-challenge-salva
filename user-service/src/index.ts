05d97a681e946eb9ef181499ca820a5529ca308f
Yeison Villegas <yeisonvillegas07@gmail.com>
2025-02-09 00:47:10 -0500

    Added manage 404 not found routes in user-service

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from "express";
 import { userRouter } from "./routes/user.route";
 import { GlobalErrorHanlder } from "./middleware/error-handler";
 import { AppDataSource } from "./data-source";
+import { AppError } from "./error/error-status";
 
 const app = express();
 
@@ -15,6 +16,11 @@ app.use(express.json());
 
 // Principal route
 app.use("/users", userRouter);
+
+// Manage 404 not found routes
+app.use((req, res, next) => {
+  next(new AppError("Route not found", 404));
+});
 
 // Error handler
 app.use(GlobalErrorHanlder.handleError);
