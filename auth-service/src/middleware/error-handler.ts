78c5ed7754b7d4704e37c1bc43e10ea10e556e4f
Yeison Villegas <yeison.villegas@cyttek.com>
2025-02-10 12:38:30 -0500

    View logs of error handler in console

--- auth-service/src/middleware/error-handler.ts
+++ auth-service/src/middleware/error-handler.ts
@@ -15,6 +15,7 @@ export class GlobalErrorHanlder {
         },
       });
     } else {
+      console.log("Error: ", err);
       res.status(500).json({
         errors: {
           message: "An unknown error occurred",
